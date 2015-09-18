5b020a7fbeb92ead610840ce5bf3a71ccd67f6c0
Demis Bellot <demis.bellot@gmail.com>
2015-09-18 12:37:45 -0700

    remove unnecessary noise

--- src/DefaultApp/DefaultApp/DefaultApp/js/components/hello.jsx
+++ src/DefaultApp/DefaultApp/DefaultApp/js/components/hello.jsx
@@ -1,15 +1,12 @@
-var React = React || require('react');
-var module = module || {};
-
 var HelloWorld = React.createClass({
     getInitialState: function () {
         return { greeting: '' };
     },
     handleNameChange: function (e) {
 		e.preventDefault();
-		var yourName = this.refs.txtName.getDOMNode().value.trim();
 		var $this = this;
-		$.getJSON('hello/' + yourName)
+		var yourName = e.target.value.trim();
+        $.getJSON('hello/' + yourName)
             .then(function(response) {
                 $this.setState({ greeting: response.Result });
             });
@@ -17,12 +14,10 @@ var HelloWorld = React.createClass({
 	render: function() {
 		return (
 			<div>
-				<input type="text" ref="txtName" className="form-control" placeholder="Your name"
-					   onChange={this.handleNameChange} />
+				<input type="text" className="form-control" placeholder="Your name"
+                       onChange={this.handleNameChange} />
 				<h3>{this.state.greeting}</h3>
 			</div>
 		);
 }
 });
-
-module.exports = HelloWorld
